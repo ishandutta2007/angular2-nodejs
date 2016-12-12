f6633301ea1767071415376b0539b677b5e265eb
Ishan Dutta <ishandutta2007@gmail.com>
2016-12-12 15:37:50 +0530

    upgrading to android 2.3

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var appDev = 'assets/app/';
 var appProd = 'public/js/app/';
 var vendor = 'public/js/vendor';
 
-/* JS & TS */
 var typescript = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 
@@ -14,7 +13,7 @@ var tsProject = typescript.createProject('tsconfig.json');
 gulp.task('build-ts', function () {
     return gulp.src(appDev + '**/*.ts')
         .pipe(sourcemaps.init())
-        .pipe(typescript(tsProject))
+        .pipe(tsProject(tsProject))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(appProd));
 });
@@ -30,10 +29,6 @@ gulp.task('vendor', function() {
     // Angular 2 Framework
     gulp.src('node_modules/@angular/**')
         .pipe(gulp.dest(vendor + '/@angular'));
-
-    //ES6 Shim
-    gulp.src('node_modules/es6-shim/**')
-        .pipe(gulp.dest(vendor + '/es6-shim/'));
 
     //reflect metadata
     gulp.src('node_modules/reflect-metadata/**')
